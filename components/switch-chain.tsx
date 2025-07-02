59fcef87d24a390fe8f98b9bd4720ca6d82487d0
devzoofi <zoozoofifi@outlook.com>
2025-07-02 14:02:33 +0800

    [test] optimize switch chain modal

--- components/switch-chain.tsx
+++ components/switch-chain.tsx
@@ -8,8 +8,10 @@ import { SimpleDialog } from "./simple-dialog"
 import { useConfigChains } from "./support-chains"
 import { useSetState } from "react-use"
 import { Spinner } from "./spinner"
+import { useRef } from "react"
 
 export function SwitchChain() {
+    const triggerRef = useRef<HTMLButtonElement>(null)
     const { chains, setDef } = useConfigChains()
     const chainId = useCurrentChainId()
     const ct = chains.find(item => item.id == chainId)
@@ -26,14 +28,21 @@ export function SwitchChain() {
         setSwitching({ [item.id]: true })
         switchChainAsync({ chainId: item.id }).catch(console.error).finally(() => {
             setSwitching({ [item.id]: false })
+            setDef(item.id)
+            triggerRef.current?.click()
         })
-        setDef(item.id)
     }
 
     const dis = useDisconnect()
+    const onDis = () => {
+        dis.disconnectAsync().finally(() => {
+            triggerRef.current?.click()
+        })
+    }
     if (!ct) return null
     return <SimpleDialog
         className="w-full max-w-[22.5rem] p-4 shadow-lg"
+        triggerRef={triggerRef}
         trigger={
             chainError ? <div className='flex items-center gap-2 text-sm font-bold rounded-lg cursor-pointer relative px-2 h-10 bg-red-500 text-white' >
                 Wrong network
@@ -53,8 +62,9 @@ export function SwitchChain() {
                     {active && isConnected && <span className="text-xs ml-auto">connected</span>}
                 </div>
             })}
-            {isConnected && <div className="flex items-center gap-2 p-2 rounded-lg cursor-pointer text-red-600 hover:bg-white/10" onClick={() => dis.disconnect()}>
+            {isConnected && <div className="flex items-center gap-2 p-2 rounded-lg cursor-pointer text-red-600 hover:bg-white/10" onClick={onDis}>
                 <FiLogOut /> Disconnect
+                {dis.isPending && <Spinner className="text-xl ml-auto" />}
             </div>}
         </div>
     </SimpleDialog>
