653597836d031241a458f634d7b1a41e72bff54f
devzoofi <zoozoofifi@outlook.com>
2025-11-14 14:43:04 +0800

    [test] fix ui

--- components/input-address.tsx
+++ components/input-address.tsx
@@ -21,7 +21,7 @@ export function AddressInput({
 }) {
   const inputRef = useRef<HTMLInputElement>(null)
   const addressError = value && !/^0x[a-fA-F0-9]{40}$/.test(value) ? 'Invalid address format' : ''
-  const mError = error ?? addressError
+  const mError = error || addressError
   const isError = !!mError
   return (
     <div
@@ -38,17 +38,17 @@ export function AddressInput({
           disabled={disable}
           className={cn(
             readonly ? 'bg-slate-50 cursor-not-allowed dark:bg-slate-800' : 'bg-white dark:bg-transparent',
+            'w-full h-14 text-right px-4 font-bold text-sm border-[#4A5546] border focus:border-2 text-slate-700 rounded-lg outline-none dark:text-slate-50',
             {
               'border-red-400 !border-2 focus:border-red-400': isError,
             },
-            'w-full h-14 text-right px-4 font-bold text-sm border-[#4A5546] border focus:border-2 text-slate-700 rounded-lg outline-none dark:text-slate-50',
           )}
           placeholder='0x...'
           maxLength={42}
           title=''
           readOnly={readonly}
         />
-        {isError && <div className='text-sm text-white bg-red-400 rounded right-0 bottom-0 absolute px-1 translate-y-1/4'>{error}</div>}
+        {isError && <div className='text-sm text-white bg-red-400 rounded right-0 bottom-0 absolute px-1 translate-y-1/4'>{mError}</div>}
       </div>
     </div>
   )
