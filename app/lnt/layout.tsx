71b2b806ee669df304010ef5041acd5768cc03a6
devzoofi <zoozoofifi@outlook.com>
2025-11-21 15:57:21 +0800

    [all] update chains

--- app/lnt/layout.tsx
+++ app/lnt/layout.tsx
@@ -4,12 +4,12 @@
     }
 import { Header } from "@/components/header";
 import { ConfigChainsProvider } from "@/components/support-chains";
-import { arbitrum, base, sepolia, zeroGmainnet, zeroGTestnet } from "@/config/network";
+import { arbitrum, base } from "@/config/network";
 import { ReactNode } from "react";
-import { arbitrumSepolia, bsc, bscTestnet } from "viem/chains";
+import { bsc } from "viem/chains";
 
 export default function Layout({ children }: { children: ReactNode }) {
-    return <ConfigChainsProvider chains={[base.id, sepolia.id, zeroGTestnet.id, zeroGmainnet.id, arbitrum.id, bsc.id, bscTestnet.id, arbitrumSepolia.id]}>
+    return <ConfigChainsProvider chains={[base.id, arbitrum.id, bsc.id]}>
         <Header />
         {children}
     </ConfigChainsProvider>
