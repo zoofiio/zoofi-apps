f51b5c72f699d4300db49073cefc41e5153c79cf
devzoofi <zoozoofifi@outlook.com>
2026-01-22 14:16:36 +0800

    [docker] root layout dynamic

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,6 +42,8 @@ export const metadata: Metadata = {
 
 }
 
+export const dynamic = 'force-dynamic'
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en' className='font-def' suppressHydrationWarning>
