a56c2beeb97e0cfdc97acc0c833a16cda83a6a79
devzoofi <zoozoofifi@outlook.com>
2026-01-23 12:22:15 +0800

    [docker] fix cache

--- lib/cache.ts
+++ lib/cache.ts
@@ -1,20 +1,23 @@
 import { retry } from "es-toolkit"
 
-const cacheMap = new Map<string, { get: () => Promise<any>, promise: Promise<any>, time?: number, data?: any }>()
-export async function cacheGet<T>(key: string, getFn: () => Promise<T>, cacheTime: number = 3000) {
+const cacheMap = new Map<string, { promise: Promise<any>, status: 'pending' | 'fullfild' | 'rejected', createTime: number }>()
+
+export function cacheGet<T>(key: string, getFn: () => Promise<T>, cacheTime: number = 3000) {
     let cache = cacheMap.get(key)
-    if (!cache) {
-        const doGet = async () => {
-            const data = await retry(() => getFn(), { retries: 3, delay: 1000 })
-            // cache
-            cacheMap.set(key, { ...cache!, time: Date.now(), data })
-            return data
+    if (!cache || cache.status == 'rejected' || (Date.now() - cache.createTime) > cacheTime) {
+        cache = {
+            createTime: Date.now(),
+            status: 'pending',
+            promise: retry(() => getFn(), { retries: 3, delay: 1000 })
+                .then((data) => {
+                    cache!.status = 'fullfild';
+                    return data
+                }).catch(e => {
+                    cache!.status = 'rejected';
+                    throw e
+                }),
         }
-        cache = { get: doGet, promise: doGet() }
         cacheMap.set(key, cache)
-    }
-    if (cache.time && cache.data && (Date.now() - cache.time) < cacheTime) {
-        return cache.data as T
     }
     return cache.promise as Promise<T>
 }
